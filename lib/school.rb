bd021bceeb662f89db6e603fa9bc8e447133a012
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-04-27 19:46:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_accessor :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+ 
+  ROSTER = {}
+ 
+  def roster
+    return ROSTER
+  end
+  
+  def add_student(name, grade)
+  end
+   
+  def grade
+  end
+  
+  def sort
+  end
+  
+end
